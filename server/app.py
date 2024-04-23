65f1294badd95b83b80bb053c198bd1f87e45b7d
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-04-23 09:50:09 -0600

    solution

--- server/app.py
+++ server/app.py
@@ -21,6 +21,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    # Earthquake.query.filter_by(id = id).first()
+    eq = Earthquake.query.filter(Earthquake.id == id).first()
+    if eq:
+        return eq.to_dict(),200
+    else:
+        return {
+                "message": f"Earthquake {id} not found."
+            },404
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    magnitude_eqs = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    r_l = []
+    for eq in magnitude_eqs:
+        r_l.append(eq.to_dict())
+    print(r_l)
+    return {
+        "count": len(magnitude_eqs),
+        "quakes": [eq.to_dict() for eq in magnitude_eqs]
+    }, 200
 
 
 if __name__ == '__main__':
