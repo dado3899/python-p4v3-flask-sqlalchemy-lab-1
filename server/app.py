1f241781a26886b3fd4a173f1f341b591931669f
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-08-05 16:04:51 -0600

    Solution

--- server/app.py
+++ server/app.py
@@ -20,6 +20,31 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    e_by_id = Earthquake.query.filter(Earthquake.id == id).first()
+    if e_by_id:
+        return e_by_id.to_dict(), 200
+    else:
+        return {
+            "message": f"Earthquake {id} not found."
+        }, 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    # find all that match
+    all_e = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    r_list = []
+    for e in all_e:
+        r_list.append(e.to_dict())
+    r_dict = {
+        "count": len(all_e),
+        "quakes": r_list
+    }
+    
+    return r_dict
+
 # Add views here
 
 
